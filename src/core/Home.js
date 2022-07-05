587e122e62df57da52a2a15d645ac8d5b276b177
dollor22 <technicalexpert004@gmail.com>
2022-07-05 23:49:24 +0530

    Home page conditnail rendering

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,18 +2,31 @@ import React, { useState, useEffect } from "react";
 import "../styles.css";
 import Base from "./Base";
 import Products from "../user/Products";
+import { isAutheticated } from "../auth/helper";
 export default function Home() {
 
   return (
-    <Base title="Welcome" description="to WZ Public's store">
-  
-       
-        <div className="card">
+  <Base title="Welcome" description="to WZ Public's store">
+{ !isAutheticated() && <div className="card">
   <div className="card-body">
    <p className="text-secondary">WZ Public is best place to invest your money. let's become member by buying our product</p>
   </div>
-</div>
-          {/* <Products/> */}
+</div>}
+
+{ isAutheticated() && isAutheticated().user?.invested === 0 && <div className="card">
+  <div className="card-body">
+   <Products/>
+  </div>
+</div>}
+
+{ isAutheticated() && isAutheticated().user?.invested !== 0 && <div className="card">
+  <div className="card-body">
+  <p className="text-secondary">You are allready WZPublic's family member. You can check your dashboard</p>
+  </div>
+</div>}
+
+
+         
     </Base>
   );
 }
